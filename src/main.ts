5baf6f6eafece079248ca3e97d2a4a30817e594c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-06-07 20:51:24 -0300

    feat: :safety_vest: Add a global body validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import helmet from "helmet";
+import { ValidationPipe } from "@nestjs/common";
+import { AppEnvironment } from "./env/env.validation";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -10,6 +12,15 @@ async function bootstrap() {
 
 	//SECTION: Cors config
 	app.enableCors();
+
+	//SECTION: Global validation
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			enableDebugMessages: process.env.NODE_ENV !== AppEnvironment.Production,
+		})
+	);
 
 	await app.listen(process.env.PORT || 3000);
 }
