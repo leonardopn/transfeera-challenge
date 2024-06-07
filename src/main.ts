91e5162f5a56757526de80f7c5ed945d9bdd5653
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-06-07 20:53:36 -0300

    feat(Module: Swagger): :sparkles: Add a module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from "./app.module";
 import helmet from "helmet";
 import { ValidationPipe } from "@nestjs/common";
 import { AppEnvironment } from "./env/env.validation";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -21,6 +22,19 @@ async function bootstrap() {
 			enableDebugMessages: process.env.NODE_ENV !== AppEnvironment.Production,
 		})
 	);
+
+	//SECTION: Swagger config (only in development)
+	if (process.env.NODE_ENV === AppEnvironment.Development) {
+		const config = new DocumentBuilder()
+			.setTitle("transfeera-challenge")
+			.setDescription("Api for transfeera-challenge")
+			.setVersion("1.0")
+			.build();
+
+		const document = SwaggerModule.createDocument(app, config);
+
+		SwaggerModule.setup("api", app, document);
+	}
 
 	await app.listen(process.env.PORT || 3000);
 }
