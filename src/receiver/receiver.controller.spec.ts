419ede4e34ca9bed3bbda4bd189d57749cdc56a5
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-06-09 16:21:11 -0300

    test(Module: Receiver): :white_check_mark: Add tests to check createOne controller on ReceiverController

--- src/receiver/receiver.controller.spec.ts
+++ src/receiver/receiver.controller.spec.ts
@@ -1,21 +1,61 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ReceiverController } from "./receiver.controller";
 import { ReceiverService } from "./receiver.service";
-import { DatabaseService } from "../database/database.service";
+import { CreateReceiverDto } from "./dto/create-receiver.dto";
+import { IReceiver } from "src/interfaces/Receiver";
 
 describe("ReceiverController", () => {
-	let controller: ReceiverController;
+	let receiverController: ReceiverController;
+	let receiverService: ReceiverService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [ReceiverController],
-			providers: [DatabaseService, ReceiverService],
+			providers: [
+				{
+					provide: ReceiverService,
+					useValue: {
+						createOne: jest.fn(),
+					},
+				},
+			],
 		}).compile();
 
-		controller = module.get<ReceiverController>(ReceiverController);
+		receiverController = module.get<ReceiverController>(ReceiverController);
+		receiverService = module.get<ReceiverService>(ReceiverService);
 	});
 
-	it("should be defined", () => {
-		expect(controller).toBeDefined();
+	describe("createOne", () => {
+		it("should call receiverService.createOne with correct data", async () => {
+			const createReceiverDto: CreateReceiverDto = {
+				completed_name: "John Doe",
+				cpf_cnpj: "12345678900",
+				email: "teste@email.com",
+				pix_data: {
+					pix_key_type: "CPF",
+					pix_key: "111.111.111-11",
+				},
+			};
+
+			const { pix_data, ...restData } = createReceiverDto;
+
+			const result: IReceiver = {
+				id: 400,
+				created_at: new Date(),
+				updated_at: new Date(),
+				status: "Rascunho",
+				pix_key: pix_data.pix_key,
+				pix_key_type: pix_data.pix_key_type,
+				email: restData.email || "",
+				...restData,
+			};
+			jest.spyOn(receiverService, "createOne").mockResolvedValue(result);
+
+			const response = await receiverController.createOne(createReceiverDto);
+
+			expect(receiverService.createOne).toHaveBeenCalledWith(createReceiverDto);
+
+			expect(response).toBe(result);
+		});
 	});
 });
