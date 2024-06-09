6ae8aa3f4a1fe820dceb780a55b63751cf213924
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-06-09 17:22:07 -0300

    test(Module: Receiver): :white_check_mark: Add tests to check getOne service on ReceiverService

--- src/receiver/receiver.service.spec.ts
+++ src/receiver/receiver.service.spec.ts
@@ -1,19 +1,70 @@
+import { NotFoundException } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
-import { ReceiverService } from "./receiver.service";
+import { IReceiver } from "src/interfaces/Receiver";
 import { DatabaseService } from "../database/database.service";
+import { ReceiverService } from "./receiver.service";
 
 describe("ReceiverService", () => {
-	let service: ReceiverService;
+	let receiverService: ReceiverService;
+	let dbService: DatabaseService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [DatabaseService, ReceiverService],
+			providers: [
+				ReceiverService,
+				{
+					provide: DatabaseService,
+					useValue: {
+						receiver: {
+							findUnique: jest.fn(),
+						},
+					},
+				},
+			],
 		}).compile();
 
-		service = module.get<ReceiverService>(ReceiverService);
+		receiverService = module.get<ReceiverService>(ReceiverService);
+		dbService = module.get<DatabaseService>(DatabaseService);
 	});
 
-	it("should be defined", () => {
-		expect(service).toBeDefined();
+	describe("getOne", () => {
+		it("should return a receiver if found", async () => {
+			const id = 1;
+			const receiver: IReceiver = {
+				id,
+				completed_name: "John Doe",
+				cpf_cnpj: "12345678900",
+				email: "teste@email.com",
+				pix_key_type: "CPF",
+				pix_key: "111.111.111-11",
+				created_at: new Date(),
+				updated_at: new Date(),
+				status: "Rascunho",
+			};
+			jest.spyOn(dbService.receiver, "findUnique").mockResolvedValue(receiver);
+
+			const result = await receiverService.getOne(id);
+
+			expect(result).toBe(receiver);
+			expect(dbService.receiver.findUnique).toHaveBeenCalledWith({ where: { id } });
+		});
+
+		it("should return null if not found and throwError is false", async () => {
+			const id = 1;
+			jest.spyOn(dbService.receiver, "findUnique").mockResolvedValue(null);
+
+			const result = await receiverService.getOne(id);
+
+			expect(result).toBeNull();
+			expect(dbService.receiver.findUnique).toHaveBeenCalledWith({ where: { id } });
+		});
+
+		it("should throw NotFoundException if not found and throwError is true", async () => {
+			const id = 1;
+			jest.spyOn(dbService.receiver, "findUnique").mockResolvedValue(null);
+
+			await expect(receiverService.getOne(id, true)).rejects.toThrow(NotFoundException);
+			expect(dbService.receiver.findUnique).toHaveBeenCalledWith({ where: { id } });
+		});
 	});
 });
